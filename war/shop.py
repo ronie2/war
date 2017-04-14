8e1751df8e6a14599153078091a27e821ce94bc2
ronie2 <roman.nzn@gmail.com>
2017-04-14 16:12:45 +0300

    - shop module comments added

--- war/shop.py
+++ war/shop.py
@@ -7,10 +7,27 @@ from .errors import TransactionValidationError
 
 
 class WarPlanesShop(Shop):
+    """Concrete planes shop implementation"""
+
     def __init__(self, equipment):
+        """
+        Initializes shop with db. Super '__init__' method is used
+        Args:
+            equipment (dict): Equipment database like in example data
+        """
         super().__init__(equipment)
 
     def buy_plane(self, player, plane_id):
+        """
+        Buys plane for `player` by `plane_id`.
+
+        Args:
+            player (dict): Original example data `player` dict
+            plane_id (int): Plane id
+
+        Returns: None
+
+        """
         transaction = get_transaction({
             'buyer': get_buyer(player),
             'plane_id': plane_id,
@@ -25,6 +42,18 @@ class WarPlanesShop(Shop):
         self.__update_player(player, transaction.transaction)
 
     def buy_gun(self, player, plane_id, gun_id):
+        """
+        Buys gun for `player` by `gun_id` and installs it into
+        plane by `plane_id`
+
+        Args:
+            player (dict): Original example data `player` dict
+            plane_id (int): Plane id
+            gun_id (int): Gun id
+
+        Returns: None
+
+        """
         transaction = get_transaction({
             'buyer': get_buyer(player),
             'plane_id': plane_id,
@@ -41,16 +70,44 @@ class WarPlanesShop(Shop):
         self.__update_player(player, transaction.transaction)
 
     def _product_spec_by_id(self, product_id):
+        """
+        Returns product specification by id stored id DB
+
+        Args:
+            product_id (int): Product id to retrieve from DB
+
+        Returns (dict|None): Product specification. None if product not found.
+
+        """
         for product_group in self.db:
             for db_id in product_group:
                 if product_id == db_id:
                     return self.db[product_group][product_id]
 
     def _product_price_by_id(self, product_id):
+        """
+        Returns product price by id stored in DB
+
+        Args:
+            product_id (int): Product id to retrieve from DB
+
+        Returns(dict/None): Returns price dict or None if product_id not found.
+
+        """
         product_spec = self._product_spec_by_id(product_id)
         return product_spec['price']
 
     def __compatible_guns(self, plane_id):
+        """
+        Returns compatible guns set or raises KeyError if product record
+        has no `compatible_guns` key.
+
+        Args:
+            plane_id (int): Plane id
+
+        Returns (set): Set containing ids of compatible guns
+
+        """
         product_spec = self._product_spec_by_id(plane_id)
         if 'compatible_guns' not in product_spec:
             raise KeyError('Product have no "compatible_guns" key')
