92dd2ef63ab966d144d1c9760792447c7b91e080
ronie2 <roman.nzn@gmail.com>
2017-04-14 16:23:47 +0300

    - transaction module comments added

--- war/transaction.py
+++ war/transaction.py
@@ -1,16 +1,44 @@
 def _buy_plane(owner):
+    """
+    This is transaction strategy function for plane buy. It implements concrete
+    steps to commit transaction.
+
+    Args:
+        owner (Transaction): Transaction instance that will use strategy
+
+    Returns: None
+
+    """
     owner.transaction['buyer'].account.decrease(owner.transaction['price'])
     owner.transaction['buyer'].hangar.add_plane(owner.transaction['plane_id'])
 
 
 def _buy_gun(owner):
+    """
+    This is transaction strategy function for gun buy. It implements concrete
+    steps to commit transaction.
+
+    Args:
+        owner (Transaction): Transaction instance that will use strategy
+
+    Returns: None
+    """
     owner.transaction['buyer'].account.decrease(owner.transaction['price'])
     owner.transaction['buyer'].hangar.set_weapon(owner.transaction['plane_id'],
                                                  owner.transaction['gun_id'])
 
 
-# Global logic controller
 def get_transaction(transaction):
+    """
+    This is factory function for creation of a `Transaction` instances
+
+    Args:
+        transaction (dict): Dictionary with transaction details
+
+    Returns (Transaction): `Transaction` instance initialized with appropriate
+    transaction data dict and transaction strategy.
+
+    """
     # If a "gun_id" isn't present, assume plane buy
     if 'gun_id' not in transaction:
         return Transaction(_buy_plane, transaction)
@@ -20,9 +48,25 @@ def get_transaction(transaction):
 
 
 class Transaction:
+    """This class is responsible for changing buyers account and hangar"""
+
     def __init__(self, strategy, transaction):
+        """
+        Initializes Transaction with transaction strategy function and
+        transaction specific details.
+
+        Args:
+            strategy (function): Concrete function to implement transaction
+            transaction (dict): Dictionary with transaction specific details
+        """
         self._transaction_strategy = strategy
         self.transaction = transaction
 
     def commit(self):
+        """
+        Executes transaction strategy function
+
+        Returns: None
+
+        """
         self._transaction_strategy()
